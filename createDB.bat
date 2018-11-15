a7868304e65601083a54e346be2ea5be3110ffae
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2018-11-16 07:49:07 +1000

    Added env variables

--- createDB.bat
+++ createDB.bat
@@ -2,10 +2,15 @@
 
 set dbContainer=frostdb
 
+set user=frostdb
+set password=mypassword
+
+set port=8001
+
 for /F %%G IN ('docker ps -a --filter "name=%dbContainer%" --format "{{.Names}}"') DO (
     echo Removing %%G container..
     docker rm -f %%G
 )
 
 echo Creating mariadb container..
-docker run -d -p 3306:3306 --name %dbContainer% -e MYSQL_ROOT_PASSWORD=mypassword mariadb:latest
+docker run -d -p %port%:3306 --name %dbContainer% -e MYSQL_USER=%user% -e MYSQL_PASSWORD=%password% -e MYSQL_RANDOM_ROOT_PASSWORD=yes mariadb:latest
--- createDev.bat
+++ createDev.bat
@@ -4,6 +4,12 @@ set apacheContainer=frostapache
 set phpContainer=frostphp
 set networkName=frostnet
 
+set port=8000
+
+set siteDir=%CD%\..\Site
+set appDir=%siteDir%\App
+set staticDir=%siteDir%\Static
+
 for /F %%G IN ('docker ps -a --filter "name=%apacheContainer%|%phpContainer%" --format "{{.Names}}"') DO (
     echo Removing %%G container..
     docker rm -f %%G
@@ -24,15 +30,11 @@ echo Building php image..
 docker build -t php .
 popd
 
-set siteDir=%CD%\..\Site
-set appDir=%siteDir%\App
-set staticDir=%siteDir%\Static
-
 echo Creating network..
 docker network create --driver bridge %networkName%
 
 echo Creating apache container..
-docker run -d -p 8000:80 -v "%staticDir%:/var/www/public" --name %apacheContainer% --network %networkName% apache
+docker run -d -p %port%:80 -v "%staticDir%:/var/www/public" --name %apacheContainer% --network %networkName% apache
 
 echo Creating php container..
 docker run -d -v "%appDir%:/var/www" --name %phpContainer% --network %networkName% php
